c66ce8f05092392d8cf3060834f2b87820f5d441
usmailaabdoul <ismaelabdul77@gmail.com>
2021-02-10 20:34:26 +0100

    Removing chat application repositroy

--- messagingApp/src/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const createApplication = require('./app');
-
-function createServer() {
-  const app = createApplication();
-  return app;
-}
-
-module.exports = createServer;
